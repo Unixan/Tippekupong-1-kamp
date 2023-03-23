f11efba1c60e8e6924033830770f37d36bc201e6
Unixan <115973884+Unixan@users.noreply.github.com>
2023-03-23 11:01:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Tippekupong
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
+            var bet = Console.ReadLine();
+            var match = new Match(bet);
+            match.Play();
+            var result = match.RightBet() ? "vant" : "tapte";
+            Console.WriteLine($"Stillingen ble {match.Score()}.\n\nDu {result}");
+
+
+
+        }
+    }
+}
