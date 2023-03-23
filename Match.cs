161be1b498d13023974a08e1530f9849794e6fde
Unixan <115973884+Unixan@users.noreply.github.com>
2023-03-23 11:01:43 +0100

    Add project files.

---
+++ Match.cs
@@ -0,0 +1,38 @@
+﻿namespace Tippekupong
+{
+    internal class Match
+    {
+        private int _homegoals;
+        private int _awaygoals;
+        private string _bet;
+        internal bool MatchIsRunning { get; private set; }
+      
+
+        public Match(string bet)
+        {
+            _bet = bet;
+            MatchIsRunning = true;
+        }
+        public string Score()
+        {
+            return _homegoals + " - " + _awaygoals;
+        }
+
+        public bool RightBet()
+        {
+           var result = _homegoals == _awaygoals ? "U" : _homegoals > _awaygoals ? "H" : "B";
+           return _bet.Contains(result);
+        }
+        public void Play()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                var rnd = new Random();
+                var teamScore = rnd.Next(9);
+                if (teamScore > 4) _homegoals++;
+                else _awaygoals++;
+            }
+
+        }
+    }
+}
